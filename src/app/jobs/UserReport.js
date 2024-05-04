1381cd4a3554386ca7f47d97c171f6f9d3e4aba2
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2024-05-04 16:30:35 -0300

    Informações

--- src/app/jobs/UserReport.js
+++ src/app/jobs/UserReport.js
@@ -2,14 +2,14 @@ export default {
     key: "UserReport",
     options: {
         limiter: {
-            max: 1,
-            duration: 1000
+            max: 1, // Maximo de tarefa pela duration
+            duration: 1000 // Tempo para executar o max de tarefas
         },
-        delay: 5000,
-        attempts: 1
+        delay: 5000, // Tempo para realizar a tarefa
+        attempts: 1 // Vezes de re-tentativa
     },
     async handle({ data }) {
-        const { user } = data
+        const { user } = dataa
 
        console.log(user);
     }
